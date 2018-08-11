13b18fcbbca8a033acba20da48572c77657f3218
David Nascimento <davidpn.11@gmail.com>
2018-08-11 20:50:27 -0300

    diving into the spectacle

--- presentation/index.js
+++ presentation/index.js
@@ -11,53 +11,126 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Appear,
+  Image,
+  Layout,
+  Fit,
+  Fill
 } from "spectacle";
+import CodeSlide from "spectacle-code-slide";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
+import profile from "../assets/profile.jpg";
 
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quarternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={500}
+        theme={theme}
+      >
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Spectacle Boilerplate
+          </Heading>
+          <Layout>
+            <Image
+              height={300}
+              width={300}
+              src={profile}
+              fit
+              size={1}
+              style={{ borderRadius: "100%" }}
+            />
+            <List>
+              <Appear>
+                <ListItem>Who am I?</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>React - DIY</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>React - Boilerplates</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>React - THE boilerplate</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Next.js</ListItem>
+              </Appear>
+            </List>
+          </Layout>
+        </Slide>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Spectacle Boilerplate
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Quero ver se atualiza{" "}
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Teste de Heading</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={6} textColor="primary" caps>
+            Typography
+          </Heading>
+          <Heading size={1} textColor="secondary">
+            Teste de Heading
+          </Heading>
+          <Heading size={2} textColor="secondary">
+            Heading 2
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            Heading 3
+          </Heading>
+          <Heading size={4} textColor="secondary">
+            Heading 4
+          </Heading>
+          <Heading size={5} textColor="secondary">
+            Heading 5
+          </Heading>
+          <Text size={6} textColor="secondary">
+            Standard text
+          </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <Appear>
+              <ListItem>Who am I?</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>React - DIY</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>React - Boilerplates</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>React - THE boilerplate</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Next.js</ListItem>
+            </Appear>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
@@ -66,6 +139,17 @@ export default class Presentation extends React.Component {
             <Cite>Author</Cite>
           </BlockQuote>
         </Slide>
+        <CodeSlide
+          bgColor="tertiary"
+          transition={[]}
+          lang="jsx"
+          code={require("raw-loader!./code")}
+          ranges={[
+            { loc: [0, 10], title: "Walking through some code" },
+            { loc: [2, 3], title: "The Beginning" },
+            { loc: [4, 5], note: "Heres a note!" }
+          ]}
+        />
       </Deck>
     );
   }
